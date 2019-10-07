86105baeace2531cfb66ee27d49be7ba9e36dcee
EduardoArias <54510876+EduardoArias@users.noreply.github.com>
2019-10-06 21:42:13 -0500

    Update AL - 1.py

--- AL - 1.py
+++ AL - 1.py
@@ -1,22 +1,16 @@
-'''AL-1 Primos'''
-def primos_eratostenes(n):
-    primos = [True for i in range (n+1)]
-    #primos[0]=False
-    #primos[1]=False
-    for m in range(2,n+1):
-        k=2
-        while m*k <= n:
-            primos[m*k] = False
-            k += 1
-    print("Primos: ")
-    for m in range (2,n+1):
-        if primos[m] == True:
-            print(f"{m}, ",end='')
-    print("\n\nNo primos: ")
-    for m in range (2,n+1):
-        if primos[m] == False:
-            print(f"{m}, ",end='')
+def primos(p):
+    primos=[]
+    noPrimos=[]
+    j=0
+    for i in range(2,p):
+        if(i%2==0):
+            primos.append(i)
+        else:
+            noPrimos.append(i)
+    print(primos)
+    print(noPrimos)
 
-numElement=int(input("¿Hasta que numero quiere loS numeros primos?"))
-primos_eratostenes(numElement)
-print("")
+        
+
+numElement=int(input("¿Hasta que numero quiere que lo numeros primos?"))
+primos(numElement)
